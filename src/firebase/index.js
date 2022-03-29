e7e838e87fbd78933135d8cabd30794658c4939c
Jefferson Murillo <development@eticsas.co>
2022-03-29 14:12:35 -0500

    Fix: Auth casi terminado

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGHYJqLCbNssa1ilOhZS_BUnr78VU8YCQ",
+  authDomain: "vue-3-2022-b3be3.firebaseapp.com",
+  projectId: "vue-3-2022-b3be3",
+  storageBucket: "vue-3-2022-b3be3.appspot.com",
+  messagingSenderId: "931736854664",
+  appId: "1:931736854664:web:d20bb0dd625e0ad5077f3d"
+};
+
+
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export { auth };
---
+++ src/views/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <form @submit.prevent="handleSubmit">
+        <input type="email" placeholder="Ingrese email" v-model.trim="email">
+        <input type="password" placeholder="Ingrese password" v-model.trim="password">
+        <button type="submit" :disabled="userStore.loadingUser">Crear usuario</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import { useUserStore } from '../stores/user.store';
+  /* import { useRouter } from 'vue-router'; */
+
+  const userStore = useUserStore();
+  /* const router = useRouter() */
+
+  const email = ref('jeff1@test.com');
+  const password = ref('123123');
+
+  const handleSubmit = async () => {
+    /* console.log(email.value);
+    console.log(password.value); */
+    if (!email.value || password.value.length <= 5) {
+        return alert('llena los campos')
+    }
+    /* console.log('Procesando formulario'); */
+    await userStore.registerUser(email.value, password.value)
+    /* router.push('/') */
+  }
+</script>
